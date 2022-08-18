a88c7b32ebea82d6067b19990f86b319161723e9
Samuel <85618108+Sami063@users.noreply.github.com>
2022-08-18 12:58:40 +0200

    Update App.js

--- Frontend/App.js
+++ Frontend/App.js
@@ -4,14 +4,23 @@ import { createDrawerNavigator } from '@react-navigation/drawer';
 import { NavigationContainer } from '@react-navigation/native';
 import LoginForm from './Authentication/Login/LoginForm';
 import RegisterForm from './Authentication/Register/RegisterForm';
+import Discover from './Screens/Discover';
+import Browse from './Screens/User/Browse';
+import Favorite from './Screens/User/Favorite';
+import More from './Screens/User/More';
+
 
 const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Drawer.Navigator initialRouteName="Home">
-        <Drawer.Screen name="Login" component={LoginForm} />
+      <Drawer.Navigator initialRouteName="Discover">
+        <Drawer.Screen name="Discover" component={Discover} />
+        <Drawer.Screen name="Browse" component={Browse} />
+        <Drawer.Screen name="Favorite" component={Favorite} />
+        <Drawer.Screen name="More" component={More} />
+        <Drawer.Screen name="Login" component={RegisterForm} />
       </Drawer.Navigator>
     </NavigationContainer>
   );
