43b284dd1d09797c0d3cb136ff15abe8111389eb
Samuel <85618108+Sami063@users.noreply.github.com>
2022-08-22 12:48:42 +0200

    Update RegisterForm.js

--- Frontend/Authentication/Register/RegisterForm.js
+++ Frontend/Authentication/Register/RegisterForm.js
@@ -1,7 +1,8 @@
 import { StyleSheet, Text, View, SafeAreaView, TextInput, Form, onChangeText, TouchableOpacity } from 'react-native';
-import React, {useState} from 'react'
-function LoginForm() {
+import React, {useState} from 'react'; 
+import axios from 'axios';
 
+function RegisterScreen() {
     // Object 
     const [user, setUser] = useState([])
 
@@ -10,28 +11,44 @@ function LoginForm() {
     const [password, setPassword] = useState('')
     const [confirmPassword, setConfirmPassword] = useState('')
 
-    const handleUserInput = (name) => {
-        setFullName(name)
+    
+    function Login() {}
+    
+    // handleSignup
+    const Signup = () => {
+        let userData = {
+            fullName,
+            email,
+            password, 
+            confirmPassword
+        };
+        setUser([...user, userData])
+
+        // password == confirmPassword ? success : validation massage
+        if(password !== confirmPassword) {
+            // show validation message - password must match
+            if(email.exists) {
+                // email exit - user exit
+            }
+        } else {
+            // Use Axios to post data to mongo database
+        }
         
-    }
-
-    const handleUserEmail = (userEmail) => {
-        setEmail(userEmail);
-    };
-    const handleUserpwd = (pwd) => {
-        setPassword(pwd);
-    }
-    const handleUserConfirmPassword = (conf_Pwd) => {
-        setConfirmPassword(conf_Pwd)
-    }
-
-    // If the email exists in the database - validation massage 'user already exist'
-    const handleLogin = () => {
-        setUser([...user, fullName])
-        console.log(user)
+        axios.post('http://192.168.43.80:3002/register', {
+            fullName,
+            email,
+            password,
+        }).then((response) => {
+            alert('Successfully registered');
+        }).catch((err) => {
+            console.error(err);
+        })
         setFullName('');
-    } 
-
+        setEmail('');
+        setPassword('');
+        setConfirmPassword('');
+        
+    };
 
     return (
         <View style={styles.screen}>
@@ -39,37 +56,37 @@ function LoginForm() {
             <View style={styles.containerScreen}>
                 <View style={styles.inputGroup}>
                     <Text>Full name</Text>
-                   <TextInput placeholder='Your full name' value={fullName} 
-                   onChangeText={handleUserInput} style={styles.textInput}/>     
+                   <TextInput placeholder='Your full name' 
+                   onChangeText={(name) => setFullName(name)} style={styles.textInput}/>     
                 </View>
                 <View style={styles.inputGroup}>
                     <Text>Email</Text>
                    <TextInput placeholder='Your email' value={email} 
-                   onChangeText={handleUserEmail} style={styles.textInput}/>     
+                   onChangeText={(email) => setEmail(email)} style={styles.textInput}/>     
                 </View>
 
                 <View style={styles.inputGroup}>
                     <Text>Password</Text>
                    <TextInput placeholder='Your password' value={password} secureTextEntry={true} 
-                   onChangeText={handleUserpwd} style={styles.textInput}/>     
+                   onChangeText={(password) => setPassword(password)} style={styles.textInput}/>     
                 </View>
                 
                 <View style={styles.inputGroup}>
                     <Text>Confirm password</Text>
                    <TextInput placeholder='re-enter password' value={confirmPassword} secureTextEntry={true} 
-                   onChangeText={handleUserConfirmPassword} style={styles.textInput}/>     
+                   onChangeText={(confirmPassword) => setConfirmPassword(confirmPassword)} style={styles.textInput}/>     
                 </View>
 
                 {/* Redirect user to login screen after creating new account*/}
                 <View style={styles.btnLogin}>
-                    <TouchableOpacity onPress={handleLogin}>
+                    <TouchableOpacity onPress={Signup}>
                         <Text style={styles.btnText}>Create account</Text>
                     </TouchableOpacity>
                 </View>
 
                 {/* Sign up - we will link to the registration form */}
                 <View style={styles.btnRegister}>
-                    <TouchableOpacity onPress={handleLogin}>
+                    <TouchableOpacity onPress={Login}>
                         <Text style={styles.btnText}>Login</Text>
                     </TouchableOpacity>
                 </View>
@@ -127,4 +144,4 @@ const styles = StyleSheet.create({
     }
 })
 
-export default LoginForm;
+export default RegisterScreen; 
