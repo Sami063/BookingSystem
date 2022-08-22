6fe60af50a8884d85ecc14acbbfb0aaa17e8f320
Morten <at114299@edu.aarhustech.com>
2022-08-22 12:29:37 +0200

    Button navigation

---
+++ Frontend/Screens/DicoveryList.js
@@ -0,0 +1,12 @@
+import { View, Text, Button } from 'react-native';
+
+
+function DiscoveryList() {
+    return (
+        <View>
+            <Text>My list</Text>
+        </View>
+        );
+    }
+
+export default DiscoveryList;
--- Frontend/Screens/Discover.js
+++ Frontend/Screens/Discover.js
@@ -1,11 +1,71 @@
-import { View, Button, Text } from 'react-native';
+import React, { useState } from 'react';
+import {  Button, Link, StyleSheet, TextInput, Text, View } from 'react-native';
+import DiscoveryList from './DicoveryList'
 
-function Discover({ navigation }) {
+function DiscoverScreen({ navigation }) {
+    const [text, setText] = useState('');
+
     return (
         <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>A list of all resturants !</Text>
+            <Text>Hello there!</Text>
+            <View style = {styles.textInput}>
+                <TextInput
+                    style={{height: 40}}
+                    placeholder="Input city!"
+                    onChangeText={newText => setText(newText)}
+                    defaultValue={text}
+                />
+                <Button
+                    onPress={() => navigation.navigate('DiscoveryList')}
+                    title="Go to List"
+                />
+            </View>
         </View>
     );
 }
 
-export default Discover
+const styles = StyleSheet.create({
+    screen: {
+        backgroundColor: '#15b103f6',
+        height: '100%',
+    }, 
+    containerScreen: {
+        backgroundColor: 'white',
+        height: '80%',
+        marginTop: '50%',
+        borderRadius: 15,
+    },
+    inputGroup: {
+        marginTop: 15,
+        marginLeft: 10,
+    }, 
+    textInput: {
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        width: '95%',
+    },
+    btnLogin: {
+        backgroundColor: 'green',
+        width: '90%',
+        height: 30,
+        marginLeft: 15, marginTop: 25,
+        alignItems: 'center',
+        alignContent: 'center',
+        borderRadius: 5,
+    }, 
+    btnRegister: {
+        backgroundColor: 'grey',
+        width: '90%',
+        height: 30,
+        marginLeft: 15, marginTop: 25,
+        alignItems: 'center',
+        alignContent: 'center',
+        borderRadius: 5,
+    },
+    btnText: {
+        fontSize: 18,
+        color: 'white',
+    }
+})
+
+export default DiscoverScreen
